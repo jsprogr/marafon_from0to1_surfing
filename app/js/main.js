fd3a6bd25c598b12219e6fb67bb549bbbb763c05
advert31337 <advert31337@yandex.ru>
2020-04-02 20:48:33 +0300

    очередная попытка пройти хотябы 10 минут этого адского марафона

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,21 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.slider-for',
-    })
+    });
 
-    
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+      slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+    })
 });
