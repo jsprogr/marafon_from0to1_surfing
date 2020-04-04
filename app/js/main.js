684a6ec17213570f99b5be296c91bc51af1ff020
advert31337 <advert31337@yandex.ru>
2020-04-04 22:49:34 +0300

    закончили третью часть адского марафона и сделали 1 час от четвертой части

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,72 @@
 $(function(){
 
     $('.header__slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
-        asNavFor: '.slider-dots'
+      infinite: true,
+      fade: true,
+      prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+      nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+      asNavFor: '.slider-dots'
     });
 
     $('.slider-dots').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.slider-for',
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      asNavFor: '.slider-for',
     });
 
     $('.surf-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
-        asNavFor: '.slider-map',
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+      nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+      asNavFor: '.slider-map',
     });
 
     $('.slider-map').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        arrows: false,
-        asNavFor: '.surf-slider',
-        focusOnSelect: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.surf-slider',
+      focusOnSelect: true,
+    });
+
+    $('.travel__slider').slick({
+      infinite: true,
+      fade: true,
+      prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+      nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
     })
+});
+
+$('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+$('.quantity').each(function() {
+  var spinner = $(this),
+    input = spinner.find('input[type="number"]'),
+    btnUp = spinner.find('.quantity-up'),
+    btnDown = spinner.find('.quantity-down'),
+    min = input.attr('min'),
+    max = input.attr('max');
+
+  btnUp.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue >= max) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue + 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+
+  btnDown.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue <= min) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue - 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+
 });
